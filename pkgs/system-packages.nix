e2cff67e87b568fe60ed7b3227f121920d687f7c
Arved Bloecker <mail@arvedbloecker.de>
2025-06-11 17:18:19 +0200

    System PKGS

---
+++ pkgs/system-packages.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  ...
+}:
+{
+  environment.systemPackages = with pkgs; [
+    # Shell
+    fish
+    # Editor
+    helix
+    vim
+    # Nix-Tools
+    nil
+    nixfmt-rfc-style
+    # Network
+    dig
+    iftop
+    inetutils
+    lftp
+    tcpdump
+    whois
+    # File and Archive
+    binwalk
+    file
+    git
+    p7zip
+    tree
+    unzip
+    # Documentation
+    #man-pages
+    #man-pages-posix
+    # Terminal
+    tmux
+  ];
+}
