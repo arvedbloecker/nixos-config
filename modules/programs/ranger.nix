0f17da4532d257a8ac1ede058b0513d96045dbe4
arvedbloecker <git@arvedbloecker.de>
2025-09-12 18:23:57 +0200

    updated ranger preview

--- modules/programs/ranger.nix
+++ modules/programs/ranger.nix
@@ -1,13 +1,47 @@
-# Ranger is a TUI Filemanager
+{ pkgs, lib, ... }:
+
+let
+  helmRifle = ''
+    # Helix für Scripts, Text & Fallback
+    ext py,sh,js,rs,ts,java,cpp,txt,md,html = helix -- "{}"
+    mime ^text/ = helix -- "{}"
+  '';
+in
 {
-  pkgs, ...
-}:
-{
-  programs.ranger = {
-    enable = true;
-  };
+  nixpkgs.overlays = [
+    (self: super: {
+      ranger = super.ranger.overrideAttrs (oldAttrs: rec {
+        nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.ed ];
+        preConfigure = ''
+          # Bildvorschau aktivieren
+          substituteInPlace ranger/config/rc.conf \
+            --replace "set preview_images false" "set preview_images true" \
+            --replace "set preview_images_method w3m" "set preview_images_method ueberzug"
+
+          # Helix-Regeln vorne in rifle.conf einfügen
+          ed -s ranger/config/rifle.conf << 'EOF'
+0a
+${helmRifle}
+.
+w
+EOF
+        '';
+      });
+    })
+  ];
 
   environment.systemPackages = with pkgs; [
-    python3Packages.pillow
+    ranger
+    ueberzugpp
+    librsvg
+    poppler
+    ffmpegthumbnailer
+    helix
   ];
+
+  environment.variables = {
+    EDITOR = "helix";
+    VISUAL = "helix";
+    RANGER_USE_EDIT = "1";
+  };
 }
