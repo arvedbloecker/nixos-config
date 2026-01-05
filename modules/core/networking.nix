100188a276e272df73a6dd0e4dd65a1db554b08a
Arved Blöcker <contact@arvedbloecker.de>
2026-01-05 11:20:22 +0100

    :bug: Made VPN work

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -1,7 +1,14 @@
 {
+  pkgs, ...
+}:
+{
   # Activates the NetworkManager-Daemon
   networking.networkmanager = {
     enable = true;
+
+    packages = [
+      pkgs.networkmanager-openconnect
+    ];
   };
 
   # Activates Bluetooth-Support by starting the BlueZ-Service (bluetoothd)
