cbbe41f3fe84c2d9ffda20af80a8f0dc3f0e012e
Arved Blöcker <contact@arvedbloecker.de>
2026-03-19 23:26:20 +0100

    Kanshi

--- hosts/magpie-arved/kanshi.nix
+++ hosts/magpie-arved/kanshi.nix
@@ -1,77 +1,80 @@
-# Kanshi defines how displays should be ordered in relation to another. 
-# 
+# Kanshi defines how displays should be ordered in relation to another.
+#
 # Execute `niri msg outputs` to find the name of the registered screens.
-{config, username, pkgs, ...}:
+{
+  username,
+  ...
+}:
 {
   home-manager.users.${username} =
     {
-      config, ...
+      config,
+      ...
     }:
     {
       services.kanshi = {
         enable = true;
-        settings =
-          [
-            {
-              profile = {
-                name = "mobile";
-                outputs = [
-                  {
-                    criteria = "Lenovo Group Limited 0x414B Unknown";
-                    mode = "2880x1800";
-                    status = "enable";
-                    scale = 1.4;
-                    position = "0,0";
-                  }
-                ];
-              };
-            }  
-            {
-              profile = {
-                # Laptop screen below external Screen
-                name = "home";
-                outputs = [
-                  {
-                    criteria = "Lenovo Group Limited 0x414B Unknown";
-                    mode = "2880x1800";
-                    status = "enable";
-                    scale = 1.5;
-                    position = "${builtins.toString ((2560 - 1920) / 2)},${builtins.toString 1440}";
-                  }
-                  {
-                    criteria = "Lenovo Group Limited Q27q-20 UPP023CY";
-                    mode = "2560x1440";
-                    status = "enable";
-                    scale = 1.0;
-                    position = "0,0";
-                  }
-                ];
-              };
-            }
-            {
-              profile = {
-                # Laptop screen below external Screen
-                name = "sra-lab-pc30";
-                outputs = [
-                  {
-                    criteria = "Lenovo Group Limited 0x414B Unknown";
-                    mode = "2880x1800";
-                    status = "enable";
-                    scale = 1.5;
-                    # 1920 comes from 2880 / 1.5 (Scaling) = 1920
-                    position = "${builtins.toString ((2560 - 1920) / 2)},${builtins.toString 1440}";
-                  }
-                  {
-                    criteria = "Dell Inc. DELL U2715H GH85D86M06FS";
-                    mode = "2560x1440";
-                    status = "enable";
-                    scale = 1.0;
-                    position = "0,0";
-                  }
-                ];
-              };
-            }
-          ];
+        settings = [
+          {
+            profile = {
+              name = "mobile";
+              outputs = [
+                {
+                  criteria = "Lenovo Group Limited 0x414B Unknown";
+                  mode = "2880x1800";
+                  status = "enable";
+                  scale = 1.4;
+                  position = "0,0";
+                }
+              ];
+            };
+          }
+          {
+            profile = {
+              # Laptop screen below external Screen
+              name = "home";
+              outputs = [
+                {
+                  criteria = "Lenovo Group Limited 0x414B Unknown";
+                  mode = "2880x1800";
+                  status = "enable";
+                  scale = 1.5;
+                  position = "${builtins.toString ((2560 - 1920) / 2)},${builtins.toString 1440}";
+                }
+                {
+                  criteria = "Lenovo Group Limited Q27q-20 UPP023CY";
+                  mode = "2560x1440";
+                  status = "enable";
+                  scale = 1.0;
+                  position = "0,0";
+                }
+              ];
+            };
+          }
+          {
+            profile = {
+              # Laptop screen below external Screen
+              name = "sra-lab-pc30";
+              outputs = [
+                {
+                  criteria = "Lenovo Group Limited 0x414B Unknown";
+                  mode = "2880x1800";
+                  status = "enable";
+                  scale = 1.5;
+                  # 1920 comes from 2880 / 1.5 (Scaling) = 1920
+                  position = "${builtins.toString ((2560 - 1920) / 2)},${builtins.toString 1440}";
+                }
+                {
+                  criteria = "Dell Inc. DELL U2715H GH85D86M06FS";
+                  mode = "2560x1440";
+                  status = "enable";
+                  scale = 1.0;
+                  position = "0,0";
+                }
+              ];
+            };
+          }
+        ];
       };
     };
 }
